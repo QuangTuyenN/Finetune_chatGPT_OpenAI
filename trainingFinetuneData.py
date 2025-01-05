293caabacbb3dfc6fe5898c282883bb44c0c6c7c
Quang Tuyền Nguyễn <113766087+QuangTuyenN@users.noreply.github.com>
2025-01-05 19:36:36 +0700

    Update trainingFinetuneData.py

--- trainingFinetuneData.py
+++ trainingFinetuneData.py
@@ -3,7 +3,7 @@
 # from apikey import OPENAI_API_KEY
 #
 # os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
-# file_id = "file-QvivaldXpnoFGwTna75qMf5L"
+# file_id = "file-"
 # response = openai.FineTuningJob.create(training_file=file_id,
 #                                        model="gpt-3.5-turbo")
 # print(response)
@@ -24,16 +24,16 @@ print(response_openai)
 
 # {
 #   "object": "fine_tuning.job",
-#   "id": "ftjob-eLvEAv6VEzXllDvPE9Qk45kz",
+#   "id": "ftjob-",
 #   "model": "gpt-3.5-turbo-0613",
 #   "created_at": 1692801986,
 #   "finished_at": null,
 #   "fine_tuned_model": null,
-#   "organization_id": "org-sRhvEIyDb8hsq6q04MPW5ViJ",
+#   "organization_id": "org-",
 #   "result_files": [],
 #   "status": "created",
 #   "validation_file": null,
-#   "training_file": "file-yRdnc3zfVwTbvrn0pQ2dsN9I",
+#   "training_file": "file-y",
 #   "hyperparameters": {
 #     "n_epochs": 5
 #   },
@@ -41,4 +41,4 @@ print(response_openai)
 # }
 
 
-# FineTuningJob(id='ftjob-tLdjhm3wyz2Azuix0FIwv9VZ', created_at=1719389127, error=Error(code=None, message=None, param=None), fine_tuned_model=None, finished_at=None, hyperparameters=Hyperparameters(n_epochs='auto', batch_size='auto', learning_rate_multiplier='auto'), model='gpt-3.5-turbo-0125', object='fine_tuning.job', organization_id='org-UmdVlF11tv5QeH20JuAh9qKC', result_files=[], seed=2015076452, status='validating_files', trained_tokens=None, training_file='file-VFaTUgkPsXz30lfHwNNpt9kl', validation_file=None, estimated_finish=None, integrations=[], user_provided_suffix=None)
+# FineTuningJob(id='ftjob-Z', created_at=1719389127, error=Error(code=None, message=None, param=None), fine_tuned_model=None, finished_at=None, hyperparameters=Hyperparameters(n_epochs='auto', batch_size='auto', learning_rate_multiplier='auto'), model='gpt-3.5-turbo-0125', object='fine_tuning.job', organization_id='org-UmdVlF11tv5QeH20JuAh9qKC', result_files=[], seed=2015076452, status='validating_files', trained_tokens=None, training_file='file-VFaTUgkPsXz30lfHwNNpt9kl', validation_file=None, estimated_finish=None, integrations=[], user_provided_suffix=None)
